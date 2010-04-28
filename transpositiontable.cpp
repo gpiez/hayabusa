48fe76938c01b8ce5dad9d352fb12d4d04ebf1b6
Gunther Piez <gpiez@web.de>
2010-04-28 11:20:42 +0200

    Popcount for a random key should be around 32 and not 16. This improves hash efficiency greatly and reduces collisions.

--- transpositiontable.cpp
+++ transpositiontable.cpp
@@ -13,15 +13,26 @@ namespace Zobrist {
 
 KeyTable zobrist;
 
-void init() {
+void initTables() {
 	srand(1);
-	for (unsigned p = -nPieces; p <= nPieces; p++)
-		if (p) for (unsigned int i = 0; i < nSquares; ++i) {
-			uint64_t r;
-			do {
-				r = (uint64_t) rand() ^ (uint64_t) rand() << 8 ^ (uint64_t) rand() << 16 ^ (uint64_t) rand() << 24 ^ (uint64_t) rand() << 32 ^ (uint64_t) rand() << 40 ^ (uint64_t) rand() << 48 ^ (uint64_t) rand() << 56;
-			} while (popcount(r) >= 14 && popcount(r) <= 17);
-			zobrist[p][i] = r;
+	for (int p = -nPieces; p <= (signed int)nPieces; p++)
+		for (unsigned int i = 0; i < nSquares; ++i)
+			if (p) {
+				uint64_t r;
+				do {
+					r = (uint64_t) rand()
+					^ (uint64_t) rand() << 8
+					^ (uint64_t) rand() << 16
+					^ (uint64_t) rand() << 24
+					^ (uint64_t) rand() << 32
+					^ (uint64_t) rand() << 40
+					^ (uint64_t) rand() << 48
+					^ (uint64_t) rand() << 56;
+				} while (popcount(r) >= 30 && popcount(r) <= 33);
+				zobrist[p+nPieces][i] = r;
+			} else {
+				zobrist[p+nPieces][i] = 0;				
+			}
 		}
 }
 }
