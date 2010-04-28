734df0fc2d93120ea2f6ba0fc7dbb1363f2ce80e
Gunther Piez <gpiez@web.de>
2010-04-28 18:03:39 +0200

    fixed operator |=, added more typesafeness, fixed PerftEntry

--- ttentry.h
+++ ttentry.h
@@ -14,17 +14,22 @@ typedef uint64_t Key;
 
 template <unsigned int pos, unsigned int width, typename T>
 struct Bitfield {
-	static const unsigned int start = pos / ( CHAR_BIT * sizeof(T));
-	static const unsigned int startpos = pos % ( CHAR_BIT * sizeof(T));
-	static const unsigned int end = (pos+width-1)/( CHAR_BIT * sizeof(T));
+	static const unsigned int tBits = CHAR_BIT * sizeof(T);
+	static const unsigned int start = pos / tBits;
+	static const unsigned int startpos = pos % tBits;
+	static const unsigned int end = (pos+width-1)/ tBits;
 
 	T data[end+1];
 	operator T () const {
 		static_assert(end == start);
 		if (width == 1)		// T is a bool type
-			return data[start] & 1ULL<<startpos;
-		else
-			return data[start] >> startpos & (1ULL<<width)-1;
+			return data[start] & (T)1 << startpos;
+		else {
+			T unmasked = data[start] >> startpos;
+			if (width + startpos != tBits)
+				unmasked &= ((T)1 << width) - 1;
+			return unmasked;
+		}
 	}
 
 //	void operator = (T value) {
@@ -34,7 +39,7 @@ struct Bitfield {
 
 	void operator |= (T value) {
 		static_assert(end == start);
-		data |= value << pos;
+		data[start] |= value << pos;
 	}
 };
 
@@ -57,9 +62,14 @@ union TTEntry {
 };
 
 struct PerftEntry {
-	Key upperKey;
+	union {
+	Bitfield<0, 6, uint8_t> depth;
+	Bitfield<6, 58, Key> upperKey;
 	uint64_t data;
-
+	};
+	enum { upperShift = 6 };
+	uint64_t value;
+	
 	void zero() { data = 0; };
 };
 
