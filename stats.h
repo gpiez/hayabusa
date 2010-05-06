d6c91147548b14e05d662e6624d9999935bdda06
Gunther Piez <gpiez@web.de>
2010-05-06 09:31:02 +0200

    Added widget for stats

---
+++ stats.h
@@ -0,0 +1,18 @@
+#ifndef STATS_H_
+#define STATS_H_
+
+#ifndef PCH_H_
+#include <pch.h>
+#endif
+
+namespace Stats {
+	uint64_t	nodes;
+	uint64_t	internalNodes;
+	uint64_t	ttuse;
+	uint64_t	ttfree;
+	uint64_t	tthits;
+	
+	
+};
+
+#endif
