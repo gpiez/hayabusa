1f75efefa169b30f8d4a55ceb176367d5c6e7632
Gunther Piez <gpiez@web.de>
2010-06-01 22:19:05 +0200

    Replaced tabs with spaces

--- console.cpp
+++ console.cpp
@@ -28,50 +28,50 @@
 unsigned int splitDepth = 5;
 
 Console::Console(QCoreApplication* parent):
-	QObject(parent),
-	app(parent),
-	cin(stdin, QIODevice::ReadOnly),
-	cout(stdout, QIODevice::WriteOnly)
+    QObject(parent),
+    app(parent),
+    cin(stdin, QIODevice::ReadOnly),
+    cout(stdout, QIODevice::WriteOnly)
 {
-	BoardBase::initTables();
-	ColoredBoard<White>::initTables();
-	ColoredBoard<Black>::initTables();
-	WorkThread::init();
-	board = new RootBoard(this);
-	board->setup();
-	
-	dispatcher["perft"] = &Console::perft;
-	dispatcher["divide"] = &Console::divide;
-	dispatcher["uci"] = &Console::uci;
-	dispatcher["debug"] = &Console::debug;
-	dispatcher["isready"] = &Console::isready;
-	dispatcher["setoption"] = &Console::setoption;
-	dispatcher["register"] = &Console::reg;
-	dispatcher["ucinewgame"] = &Console::ucinewgame;
-	dispatcher["position"] = &Console::position;
-	dispatcher["go"] = &Console::go;
-	dispatcher["stop"] = &Console::stop;
-	dispatcher["ponderhit"] = &Console::ponderhit;
-	dispatcher["quit"] = &Console::quit;
-	
-	notifier = new QSocketNotifier(STDIN_FILENO, QSocketNotifier::Read, this);
-	connect(notifier, SIGNAL(activated(int)), this, SLOT(dataArrived()));
-	connect(this, SIGNAL(signalSend(QString)), this, SLOT(privateSend(QString)));
-	
-	QStringList args = QCoreApplication::arguments();
-	args.removeFirst();
-	QString argStr = args.join(" ");
-	QStringList cmdsList = argStr.split(":");
-	foreach(QString cmdStr, cmdsList) {
-		cmdStr = cmdStr.simplified();
-		QStringList cmds = cmdStr.split(" ");
-		if (cmds.isEmpty()) break;
-		if (dispatcher.contains(cmds[0]))
-			(this->*(dispatcher[cmds[0]]))(cmds);
-		else
-			tryMove(cmds);
-		
-	}
+    BoardBase::initTables();
+    ColoredBoard<White>::initTables();
+    ColoredBoard<Black>::initTables();
+    WorkThread::init();
+    board = new RootBoard(this);
+    board->setup();
+    
+    dispatcher["perft"] = &Console::perft;
+    dispatcher["divide"] = &Console::divide;
+    dispatcher["uci"] = &Console::uci;
+    dispatcher["debug"] = &Console::debug;
+    dispatcher["isready"] = &Console::isready;
+    dispatcher["setoption"] = &Console::setoption;
+    dispatcher["register"] = &Console::reg;
+    dispatcher["ucinewgame"] = &Console::ucinewgame;
+    dispatcher["position"] = &Console::position;
+    dispatcher["go"] = &Console::go;
+    dispatcher["stop"] = &Console::stop;
+    dispatcher["ponderhit"] = &Console::ponderhit;
+    dispatcher["quit"] = &Console::quit;
+    
+    notifier = new QSocketNotifier(STDIN_FILENO, QSocketNotifier::Read, this);
+    connect(notifier, SIGNAL(activated(int)), this, SLOT(dataArrived()));
+    connect(this, SIGNAL(signalSend(QString)), this, SLOT(privateSend(QString)));
+    
+    QStringList args = QCoreApplication::arguments();
+    args.removeFirst();
+    QString argStr = args.join(" ");
+    QStringList cmdsList = argStr.split(":");
+    foreach(QString cmdStr, cmdsList) {
+        cmdStr = cmdStr.simplified();
+        QStringList cmds = cmdStr.split(" ");
+        if (cmds.isEmpty()) break;
+        if (dispatcher.contains(cmds[0]))
+            (this->*(dispatcher[cmds[0]]))(cmds);
+        else
+            tryMove(cmds);
+    
+    }
 }
 
 Console::~Console()
@@ -79,101 +79,101 @@ Console::~Console()
 }
 
 void Console::getResult(QString result) {
-	answer = result;
-	cout << result << endl;
+    answer = result;
+    cout << result << endl;
 }
 
 void Console::dataArrived() {
-	notifier->setEnabled(false);
+    notifier->setEnabled(false);
     QString temp = cin.readLine();
     if (!temp.isEmpty()) {
-    	QStringList cmds = temp.split(' ');
-    	if (dispatcher.contains(cmds[0])) {
-    		(this->*(dispatcher[cmds[0]]))(cmds);
-    	} else {
-    		tryMove(cmds);
-    	}
+        QStringList cmds = temp.split(' ');
+        if (dispatcher.contains(cmds[0])) {
+            (this->*(dispatcher[cmds[0]]))(cmds);
+        } else {
+            tryMove(cmds);
+        }
 
     }
     QTimer::singleShot(50, this, SLOT(delayedEnable()));
 }
 
 void Console::delayedEnable() {
-	notifier->setEnabled(true);
+    notifier->setEnabled(true);
 }
 
 void Console::tryMove(QStringList cmd) {
-	qDebug() << "trymove" << cmd;
+    qDebug() << "trymove" << cmd;
 }
 
 void Console::perft(QStringList cmds) {
-	board->perft(cmds[1].toInt()); 
+    board->perft(cmds[1].toInt());
 }
 
 void Console::divide(QStringList cmds) {
-	board->divide(cmds[1].toInt());
+    board->divide(cmds[1].toInt());
 }
 
 void Console::uci(QStringList /*cmds*/) {
-	send("id name hayabusa 0.1");
-	send("author Gunther Piez");
+    send("id name hayabusa 0.1");
+    send("author Gunther Piez");
 }
 
 void Console::debug(QStringList cmds) {
-	if (cmds[1] == "on") 
-		debugMode = true;
-	else if (cmds[1]=="off") 
-		debugMode = false;
-	else
-		send("info string usage: debug on|off");
+    if (cmds[1] == "on")
+        debugMode = true;
+    else if (cmds[1]=="off")
+        debugMode = false;
+    else
+        send("info string usage: debug on|off");
 }
 
 // engine is always ready as soon as the command dispatcher is working.
 void Console::isready(QStringList /*cmds*/) {
-	cout << "readyok" << endl << flush;
+    cout << "readyok" << endl << flush;
 }
 
 void Console::setoption(QStringList cmds) {
-	const QHash<QString, QStringList> o = parse(cmds, QStringList() << "name" << "value");
-	QString name = o["name"].join(" ").toLower();
-	if (!name.isEmpty()) {
-		QString data = o["value"].join(" ").toLower();
-		if (name == "splitdepth") {
-			splitDepth = data.toInt();
-		}
-	}
+    const QHash<QString, QStringList> o = parse(cmds, QStringList() << "name" << "value");
+    QString name = o["name"].join(" ").toLower();
+    if (!name.isEmpty()) {
+        QString data = o["value"].join(" ").toLower();
+        if (name == "splitdepth") {
+            splitDepth = data.toInt();
+        }
+    }
 }
 
 void Console::reg(QStringList /*cmds*/) {
 }
 
 void Console::ucinewgame(QStringList /*cmds*/) {
-	WorkThread::stopAll();
-	board->ttClear();
+    WorkThread::stopAll();
+    board->ttClear();
 }
 
 void Console::position(QStringList cmds) {
-	WorkThread::stopAll();
-	int m=cmds.indexOf("moves");
-	if (cmds[1] == "startpos")
-		board->setup();
-	else if (cmds[1] == "fen") {
-		QStringList fen = cmds.mid(2, m-2);
-		board->setup(fen.join(" "));
-	}
-	if (m>0) {
-		QStringList moves = cmds.mid(m+1);
-		foreach(QString move, cmds)
-			; //TODO
-	}
+    WorkThread::stopAll();
+    int m=cmds.indexOf("moves");
+    if (cmds[1] == "startpos")
+        board->setup();
+    else if (cmds[1] == "fen") {
+        QStringList fen = cmds.mid(2, m-2);
+        board->setup(fen.join(" "));
+    }
+    if (m>0) {
+        QStringList moves = cmds.mid(m+1);
+        foreach(QString move, cmds)
+            ; //TODO
+    }
 }
 
 void Console::go(QStringList cmds) {
-	WorkThread::stopAll();
-	QHash<QString, QStringList> subCmds = parse(cmds, QStringList() << "searchmoves"
-	<< "ponder" << "wtime" << "btime" << "winc" << "binc" << "movestogo" << "depth"
-	<< "nodes" << "mate" << "movetime" << "infinite");
-	board->go(cmds);
+    WorkThread::stopAll();
+    QHash<QString, QStringList> subCmds = parse(cmds, QStringList() << "searchmoves"
+    << "ponder" << "wtime" << "btime" << "winc" << "binc" << "movestogo" << "depth"
+    << "nodes" << "mate" << "movetime" << "infinite");
+    board->go(cmds);
 }
 
 void Console::stop(QStringList /*cmds*/) {
@@ -183,50 +183,50 @@ void Console::ponderhit(QStringList /*cmds*/) {
 }
 
 void Console::quit(QStringList /*cmds*/) {
-	app->quit();
+    app->quit();
 }
 
 QString Console::getAnswer() {
-	answer = QString("");
-	while(answer == "") {
-		app->processEvents();
-		sleep(1);
-	}
-	return answer;
+    answer = QString("");
+    while(answer == "") {
+        app->processEvents();
+        sleep(1);
+    }
+    return answer;
 }
 
 void Console::info(int depth, int seldepth, uint64_t time, uint64_t nodes,
-				   QString pv, RawScore score, Move currMove, int currMoveNumber,
-				   int hashfull, int nps, int tbhits, int cpuload, QString currline) {
-	emit signalInfo(depth, seldepth, time, nodes,
-				   pv, score, currMove, currMoveNumber,
-				   hashfull, nps, tbhits, cpuload, currline);
+                   QString pv, RawScore score, Move currMove, int currMoveNumber,
+                   int hashfull, int nps, int tbhits, int cpuload, QString currline) {
+    emit signalInfo(depth, seldepth, time, nodes,
+                   pv, score, currMove, currMoveNumber,
+                   hashfull, nps, tbhits, cpuload, currline);
 }
 
 void Console::iterationDone(unsigned int depth, uint64_t nodes, QString line, int bestScore) {
-	emit signalIterationDone(depth, nodes, line, bestScore);
+    emit signalIterationDone(depth, nodes, line, bestScore);
 }
 
 void Console::privateSend(QString str)
 {
-	cout << str << endl;
+    cout << str << endl;
 }
 
 void Console::send(QString str) {
-	emit signalSend(str);
+    emit signalSend(str);
 }
 
 QHash<QString, QStringList> Console::parse(QStringList cmds, QStringList tokens) {
-	QMap<int, QString> tokenPositions;
-	foreach(QString token, tokens)
-		if (cmds.indexOf(token)>0)
-			tokenPositions[cmds.indexOf(token)] = token;
-	tokenPositions[9999] = "";
-	
-	QHash<QString, QStringList> tokenValues;
-	for(int i=0; i<tokenPositions.keys().count()-1; ++i) {
-		tokenValues[tokenPositions.values().at(i)] =
-			cmds.mid(tokenPositions.keys().at(i)+1, tokenPositions.keys().at(i+1)-tokenPositions.keys().at(i)-1);
-	}
-	return tokenValues;
+    QMap<int, QString> tokenPositions;
+    foreach(QString token, tokens)
+        if (cmds.indexOf(token)>0)
+            tokenPositions[cmds.indexOf(token)] = token;
+    tokenPositions[9999] = "";
+    
+    QHash<QString, QStringList> tokenValues;
+    for(int i=0; i<tokenPositions.keys().count()-1; ++i) {
+        tokenValues[tokenPositions.values().at(i)] =
+            cmds.mid(tokenPositions.keys().at(i)+1, tokenPositions.keys().at(i+1)-tokenPositions.keys().at(i)-1);
+    }
+    return tokenValues;
 }
