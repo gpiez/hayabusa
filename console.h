4339b1759be956064db6c247cf0bf5480c5e582a
Gunther Piez <gpiez@web.de>
2010-06-01 22:29:50 +0200

    Replaced tabs with spaces

--- console.h
+++ console.h
@@ -31,58 +31,58 @@ class WorkThread;
 class RootBoard;
 
 class Console: public QObject {
-	Q_OBJECT
-	friend class TestRootBoard;
-	void perft(QStringList);
-	void divide(QStringList);
-	void tryMove(QStringList);
-	void quit(QStringList);
-	void ponderhit(QStringList);
-	void stop(QStringList);
-	void go(QStringList);
-	void position(QStringList);
-	void ucinewgame(QStringList);
-	void reg(QStringList);
-	void setoption(QStringList);
-	void isready(QStringList);
-	void uci(QStringList);
-	void debug(QStringList);
+    Q_OBJECT
+    friend class TestRootBoard;
+    void perft(QStringList);
+    void divide(QStringList);
+    void tryMove(QStringList);
+    void quit(QStringList);
+    void ponderhit(QStringList);
+    void stop(QStringList);
+    void go(QStringList);
+    void position(QStringList);
+    void ucinewgame(QStringList);
+    void reg(QStringList);
+    void setoption(QStringList);
+    void isready(QStringList);
+    void uci(QStringList);
+    void debug(QStringList);
 private slots:
-	void privateSend(QString);
-	
+    void privateSend(QString);
+    
 private:
-	QCoreApplication* app;
-	RootBoard* board;
-	QString answer;
+    QCoreApplication* app;
+    RootBoard* board;
+    QString answer;
     bool debugMode;
     QMap<QString, QString> option;
-	QHash<QString, QStringList> parse(QStringList, QStringList);
-	QMap<QString, void (Console::*)(QStringList)> dispatcher;
-	
+    QHash<QString, QStringList> parse(QStringList, QStringList);
+    QMap<QString, void (Console::*)(QStringList)> dispatcher;
+    
 public:
-	QTextStream cin;
-	QTextStream cout;
-	QSocketNotifier *notifier;
+    QTextStream cin;
+    QTextStream cout;
+    QSocketNotifier *notifier;
 
-	Console(QCoreApplication* parent);
-	virtual ~Console();
+    Console(QCoreApplication* parent);
+    virtual ~Console();
     void iterationDone(unsigned int depth, uint64_t nodes, QString line, int bestScore);
-	void info(int depth, int seldepth, uint64_t time, uint64_t nodes,
-						 QString pv, RawScore score, Move currMove, int currMoveNumber,
-						 int hashfull, int nps, int tbhits, int cpuload, QString currline);
-	void send(QString);
+    void info(int depth, int seldepth, uint64_t time, uint64_t nodes,
+                         QString pv, RawScore score, Move currMove, int currMoveNumber,
+                         int hashfull, int nps, int tbhits, int cpuload, QString currline);
+    void send(QString);
 
 public slots:
-	void dataArrived();
-	void delayedEnable();
-	void getResult(QString);
-	QString getAnswer();
-	
+    void dataArrived();
+    void delayedEnable();
+    void getResult(QString);
+    QString getAnswer();
+    
 signals:
-	void signalSend(QString);
-	void signalInfo(int depth, int seldepth, uint64_t time, uint64_t nodes,
-						 QString pv, RawScore score, Move currMove, int currMoveNumber,
-						 int hashfull, int nps, int tbhits, int cpuload, QString currline);						 
+    void signalSend(QString);
+    void signalInfo(int depth, int seldepth, uint64_t time, uint64_t nodes,
+                         QString pv, RawScore score, Move currMove, int currMoveNumber,
+                         int hashfull, int nps, int tbhits, int cpuload, QString currline);
     void signalIterationDone(unsigned int depth, uint64_t nodes, QString line, int bestScore);
 };
 
