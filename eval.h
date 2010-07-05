500c42e3e2c8d846f315638d4bf49f4e8f1fcd29
Gunther Piez <gpiez@web.de>
2010-07-05 22:02:16 +0200

    introduced CompoundScore

--- eval.h
+++ eval.h
@@ -39,6 +39,8 @@
  * v = (sum over pieces (v_inhPieces * n_inhPieces) + n_totalInhPieces^2 * v_totalInh) * squareValue
  * squareValue = attack_king(dist_king) + attack_piece(dist_piece) + attack_pawn(dist_pawn) + const
  */
+class BoardBase;
+
 struct SquareEval {
     RawScore    controls[nPieces*2 + 1];    //piece may move there unharmed.
     RawScore    attacks[nPieces*2 + 1];        //piece attacks/defends square.
@@ -47,16 +49,36 @@ struct SquareEval {
 template<typename T>
 void sigmoid(T& p, double start, double end, double dcenter = 0, double width = 1.5986 );
 
-struct CompoundScore {
+union CompoundScore {
+    struct {
 	RawScore    opening;
 	RawScore    endgame;
+    };
+    int data;
+    
+    CompoundScore operator + (const CompoundScore& x) const {
+        CompoundScore temp;
+        temp.data = x.data + data;
+        return temp;
+    }
+    CompoundScore operator - () const {
+        CompoundScore temp;
+        temp.opening = -opening;
+        temp.endgame = -endgame;
+        return temp;
+    }
+    int calc(const BoardBase&, const Eval&) const {
+        return opening;
+    }
+    void operator = (int x) {
+        opening = x;
+        endgame = x;
+    }
 };
 
 union KeyScore {
     struct {
-    	RawScore    score;
-    	RawScore    endgame;
-//    	CompoundScore  score;
+    	CompoundScore  score;
         PawnKey        pawnKey;
         Key            key;
     };
@@ -88,19 +110,20 @@ class BoardBase;
 class Eval {
     KeyScore zobristPieceSquare[nTotalPieces][nSquares];
     
-    static RawScore pawn, knight, bishop, rook, queen;
-    static RawScore bishopPair;
-    static RawScore knightAlone;
-    static RawScore bishopAlone;
-    static RawScore n_p_ram[9], n_p[17];
-    static RawScore b_bad_own_p[9], b_p[17], b_p_ram[9], bpair_p[17];
-    static RawScore r_p[17];
-    static RawScore q_p[17];
-    static RawScore pawnBackward;
-    static RawScore pawnBackwardOpen;
-    static RawScore pawnIsolated;    
-    static RawScore pawnHalfIsolated;
-    static RawScore pawnPasser[8];
+    static CompoundScore pawn, knight, bishop, rook, queen;
+    static CompoundScore bishopPair;
+    static CompoundScore knightAlone;
+    static CompoundScore bishopAlone;
+    static CompoundScore n_p_ram[9], n_p[17];
+    static CompoundScore b_bad_own_p[9], b_p[17], b_p_ram[9], bpair_p[17];
+    static CompoundScore r_p[17];
+    static CompoundScore q_p[17];
+    // needs to be the same kind of type as PawnEntry.score.
+    static int pawnBackward;
+    static int pawnBackwardOpen;
+    static int pawnIsolated;
+    static int pawnHalfIsolated;
+    static int pawnPasser[8];
     
     TranspositionTable<PawnEntry, 4, PawnKey>* pt;
     
@@ -117,32 +140,28 @@ class Eval {
     unsigned int attackHash( unsigned int  pos );
     int squareControl();
 
-    template<Colors C>
-    RawScore mobility(const BoardBase&) const;
-    RawScore defense(const BoardBase&) const;
-    template<Colors C>
-    RawScore attack(const BoardBase&) const;
-    RawScore pieces(const PieceList&, int) const;
-    RawScore pawns(const BoardBase&) const;
+    template<Colors C> int mobility(const BoardBase&) const;
+    template<Colors C> int attack(const BoardBase&) const;
+    int pieces(const BoardBase&) const;
+    int pawns(const BoardBase&) const;
     template<Colors C> void EvalMate(const ColoredBoard<C>&) const;
-    template<Colors C>
-    void mobilityRestrictions(const BoardBase &b, uint64_t (&restrictions)[nColors][nPieces+1]) const;
-    template<Colors C>
-    RawScore mobilityValue( const BoardBase &b, const uint64_t (&restrictions)[nColors][nPieces+1]) const;
+    template<Colors C> void mobilityRestrictions(const BoardBase &b, uint64_t (&restrictions)[nColors][nPieces+1]) const;
+    template<Colors C> int mobility( const BoardBase &b, const uint64_t (&restrictions)[nColors][nPieces+1]) const;
 public:
     Eval();
-    RawScore eval(const BoardBase&) const;  ///TODO should be template<C>
-    RawScore getPS(int8_t piece, uint8_t square) const {
+    int eval(const BoardBase&) const;
+    CompoundScore getPS(int8_t piece, uint8_t square) const {
         ASSERT(square < nSquares);
         ASSERT(piece >= (signed)-nPieces && piece <= (signed)nPieces);
         return zobristPieceSquare[piece+nPieces][square].score;
     }
-    RawScore& getPS(int8_t piece, uint8_t square) {
+    CompoundScore& getPS(int8_t piece, uint8_t square) {
         ASSERT(square < nSquares);
         ASSERT(piece >= (signed)-nPieces && piece <= (signed)nPieces);
         return zobristPieceSquare[piece+nPieces][square].score;
     }
     __v8hi getKSVector(int8_t piece, uint8_t square) const {
+        static_assert(sizeof(KeyScore) == sizeof(__v8hi), "Structure size error");
         ASSERT(square < nSquares);
         ASSERT(piece >= (signed)-nPieces && piece <= (signed)nPieces);
         return zobristPieceSquare[piece+nPieces][square].vector;
@@ -176,7 +195,7 @@ void printSigmoid(T& p, QString str) {
     QTextStream xout(stderr);
     xout << qSetFieldWidth(16) << str;
     for (unsigned int i = 0; i <= n; ++i) {
-        xout << qSetFieldWidth(4) << p[i];
+        /*xout << qSetFieldWidth(4) << p[i]*/;
     }
     xout << endl;
 }
