a8d7a27b9355238e072a051bf8d37859d66f2eb2
Gunther Piez <gpiez@web.de>
2010-09-02 21:30:54 +0200

    substitute for qstringlist

---
+++ stringlist.cpp
@@ -0,0 +1,75 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "stringlist.h"
+
+std::string StringList::join(const char* str) {
+    auto i=begin();
+    if (i == end()) return "";
+    std::string sum = *i++;
+    while (i!=end()) {
+        sum += str + *i;
+        ++i;
+    }
+    return sum;
+}
+
+StringList& StringList::operator << (const char* str) {
+    push_back(std::string(str));
+    return *this;
+}
+
+StringList split(std::string str, std::string delims) {
+    using namespace std;
+    // Skip delims at beginning, find start of first token
+    string::size_type lastPos = str.find_first_not_of(delims, 0);
+    // Find next delimiter @ end of token
+    string::size_type pos     = str.find_first_of(delims, lastPos);
+
+    // output vector
+    StringList tokens;
+
+    while (string::npos != pos || string::npos != lastPos)
+    {
+        // Found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        // Skip delims.  Note the "not_of". this is beginning of token
+        lastPos = str.find_first_not_of(delims, pos);
+        // Find next delimiter at end of token.
+        pos     = str.find_first_of(delims, lastPos);
+    }
+
+    return tokens;
+}
+
+std::string simplified(std::string str) {
+    return str;
+}
+
+std::string toLower(std::string str) {
+    return str;
+}
+
+int convert(std::string str) {
+    std::stringstream ss(str);
+    int x;
+    ss >> x;
+    return x;
+}
+
---
+++ stringlist.h
@@ -0,0 +1,38 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef STRINGLIST_H
+#define STRINGLIST_H
+
+#ifndef PCH_H_
+#include <pch.h>
+#endif
+
+class StringList: public std::vector<std::string> {
+public:
+    std::string join(const char* = "");
+    StringList& operator << (const char*);
+};
+
+StringList split(std::string str, std::string delims = std::string(" "));
+std::string simplified(std::string str);
+std::string toLower(std::string str);
+int convert(std::string str);
+
+#endif // STRINGLIST_H
