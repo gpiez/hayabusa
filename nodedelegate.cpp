5f7014dcc0e8a8c8ad3919a03029e28e3be2698a
Gunther Piez <gpiez@web.de>
2011-01-06 12:08:05 +0100

    fixed indent

--- nodedelegate.cpp
+++ nodedelegate.cpp
@@ -13,20 +13,20 @@
 
 void NodeDelegate::paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const
 {
-	const NodeItem *item = static_cast<NodeItem*>( index.internalPointer() );
-	// painter->begin();
-	QString str1, str2;
-	if ( item->move.fromto() ) {
-		str1 = QString().fromStdString(item->move.string());
+    const NodeItem *item = static_cast<NodeItem*>( index.internalPointer() );
+    // painter->begin();
+    QString str1, str2;
+    if ( item->move.fromto() ) {
+        str1 = QString().fromStdString(item->move.string());
 
-		str2 = QString( "D%4 [%1 %2]\n %3" ).arg( item->alpha ).arg( item->beta ).arg( item->bestEval ).arg( (int)item->depth );
-	} else {
-		str1 = QString( "Iteration %1" ).arg( item->ply );
-		str2 = "";
-	}
+        str2 = QString( "D%4 [%1 %2]\n %3" ).arg( item->alpha ).arg( item->beta ).arg( item->bestEval ).arg( (int)item->depth );
+    } else {
+        str1 = QString( "Iteration %1" ).arg( item->ply );
+        str2 = "";
+    }
 
-	str2+=QString( " ply: %1 " ).arg( item->ply );
-	switch(item->nodeType) {
+    str2+=QString( " ply: %1 " ).arg( item->ply );
+    switch(item->nodeType) {
     case NodePrecut1:
         str2 += "Precut1";
         break;
@@ -36,71 +36,71 @@ void NodeDelegate::paint ( QPainter * painter, const QStyleOptionViewItem & opti
     case NodePrecut3:
         str2 += "Precut3";
         break;
-	case NodeTT:
-		str2 += "TT";
-		break;
-	case NodeMate:
-		str2 += "Mate";
-		break;
-	case NodeIllegal:
-		str2 += "Illegal";
-		break;
-	case NodeRepetition:
-		str2 += "Repetition";
-		break;
-	default:
-		str2 += "Full";
-		break;
-	}
-	switch(item->searchType) {
-	case root:
-		str2 += "<Root>";
-		break;
-	case trunk:
-		str2 += "<Trunk>";
-		break;
-	case tree:
-		str2 += "<Tree>";
-		break;
-	case leaf:
-		str2 += "<Leaf>";
-		break;
-	case vein:
-		str2 += "<Vein>";
-		break;
-	}
-	if (item->flags & Threatened)
+    case NodeTT:
+        str2 += "TT";
+        break;
+    case NodeMate:
+        str2 += "Mate";
+        break;
+    case NodeIllegal:
+        str2 += "Illegal";
+        break;
+    case NodeRepetition:
+        str2 += "Repetition";
+        break;
+    default:
+        str2 += "Full";
+        break;
+    }
+    switch(item->searchType) {
+    case root:
+        str2 += "<Root>";
+        break;
+    case trunk:
+        str2 += "<Trunk>";
+        break;
+    case tree:
+        str2 += "<Tree>";
+        break;
+    case leaf:
+        str2 += "<Leaf>";
+        break;
+    case vein:
+        str2 += "<Vein>";
+        break;
+    }
+    if (item->flags & Threatened)
         str2 += QString(" TD ");
     if (item->flags & Threatening)
         str2 += QString(" TG ");
 
-	str2 += QString( " size: %2" ).arg(item->nodes);
+    str2 += QString( " size: %2" ).arg(item->nodes);
 /*
-	if ( index.parent().isValid() )
-		if ( static_cast<NodeItem*>( index.parent().internalPointer() )->bestMove.fromto() == item->move.fromto() )
-			painter->setPen( Qt::red );
+    if ( index.parent().isValid() )
+        if ( static_cast<NodeItem*>( index.parent().internalPointer() )->bestMove.fromto() == item->move.fromto() )
+            painter->setPen( Qt::red );
 */
 
-	QRect coor = option.rect;
-	if (item->moveColor == White) {
-		painter->setPen(Qt::white);
-	} else {
-		painter->setPen(Qt::black);
-	}
+    QRect coor = option.rect;
+    if (item->moveColor == White) {
+        painter->setPen(Qt::white);
+    } else {
+        painter->setPen(Qt::black);
+    }
 
-	painter->drawText( coor, Qt::AlignVCenter | Qt::AlignLeft, str1 );
-	coor.setLeft( coor.left() + 50 );
-	if (item->nodeColor == White) {
-		painter->setPen(Qt::white);
-	} else {
-		painter->setPen(Qt::black);
-	}
-	painter->drawText( coor, Qt::AlignTop | Qt::AlignLeft, str2 );
+    painter->drawText( coor, Qt::AlignVCenter | Qt::AlignLeft, str1 );
+    coor.setLeft( coor.left() + 50 );
+    if (item->nodeColor == White) {
+        painter->setPen(Qt::white);
+    } else {
+        painter->setPen(Qt::black);
+    }
+    painter->drawText( coor, Qt::AlignTop | Qt::AlignLeft, str2 );
 }
 
 QSize NodeDelegate::sizeHint ( const QStyleOptionViewItem& /*option*/, const QModelIndex& /*index*/ ) const
 {
-	return QSize( 100,40 );
+    return QSize( 100,40 );
 }
 
 #endif
