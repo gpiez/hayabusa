cb491238f2c4f682ce9675d3df5b88f0dd375f96
Gunther Piez <gpiez@web.de>
2011-01-06 12:10:53 +0100

    fixed indent

--- nodeitem.cpp
+++ nodeitem.cpp
@@ -1,4 +1,4 @@
-/*
+*/
  * nodeitem.cpp
  *
  *  Created on: 09.04.2009
@@ -16,67 +16,67 @@ std::recursive_mutex NodeItem::m;
 int NodeItem::nNodes = 0;
 
 NodeItem::NodeItem( const NodeData& nodeData_ ):
-		NodeData( nodeData_ ),
-		parent( 0 )
+        NodeData( nodeData_ ),
+        parent( 0 )
 {}
 
 NodeItem::NodeItem( const NodeData& nodeData_, NodeItem* parent_ ):
-		NodeData( nodeData_ ),
-		parent( parent_ )
+        NodeData( nodeData_ ),
+        parent( parent_ )
 {
-	id = stats.node;
+    id = stats.node;
     m.lock();
-	if (parent_)
-		parent_->appendChild( this );
-	else
-		delete this;
+    if (parent_)
+        parent_->appendChild( this );
+    else
+        delete this;
     m.unlock();
 }
 
 NodeItem::~NodeItem()
 {
-	m.lock();
-	qDeleteAll( children );
-	m.unlock();
+    m.lock();
+    qDeleteAll( children );
+    m.unlock();
 }
 
 void NodeItem::appendChild( NodeItem* item )
 {
-	m.lock();
-	children.append( item );
-	m.unlock();
+    m.lock();
+    children.append( item );
+    m.unlock();
 }
 
 const NodeItem *NodeItem::child( int row )
 {
-	return children.value( row );
+    return children.value( row );
 }
 
 NodeItem *NodeItem::lastChild(  )
 {
-	return children.last();
+    return children.last();
 }
 
 int NodeItem::childCount() const
 {
-	return children.count();
+    return children.count();
 }
 
 int NodeItem::row()
 {
-	if ( parent )
-		return parent->children.indexOf( this );
-	return 0;
+    if ( parent )
+        return parent->children.indexOf( this );
+    return 0;
 }
 
 const NodeData& NodeItem::data() const
 {
-	return ( const NodeData& )( *this );
+    return ( const NodeData& )( *this );
 }
 
 NodeItem* NodeItem::getParent() const
 {
-	return parent;
+    return parent;
 }
 
 #endif
